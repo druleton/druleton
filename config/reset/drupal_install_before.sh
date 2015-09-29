ee1a3fd11e433fc311b5b970776b65af2393f855
Peter Decuyper <github@serial-graphics.be>
2015-09-29 08:31:13 +0200

    Moved the symlink scripts to the drupal_make_after hook script.

--- config/reset/drupal_install_before.sh
+++
@@ -1,8 +0,0 @@
-
-################################################################################
-# Use this file to add custom script steps that should run before the
-# installation of Drupal is started.
-################################################################################
-
-# Reset needs the same post steps as install.
-source "$DIR_CONFIG/install/drupal_install_before.sh"
