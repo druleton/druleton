8593285a0c35f5bee9da84dc81328f9cb0ecca35
zero2one <github@serial-graphics.be>
2015-11-10 08:36:14 +0100

    Implemented the default hooks to symlink and copy the project Profiles, Modules, Themes and Libraries.

--- config/build/drupal_make_after.sh
+++ config/build/drupal_make_after.sh
@@ -5,24 +5,53 @@
 ################################################################################
 
 
-# Add symlinks from the project folder.
-markup_h1 "Copy custom modules and themes code to build."
+# Add the custom code from the project folder.
+markup_h1 "Copy project profiles, modules, themes and libraries."
+
+# Copy profiles.
+project_profiles=$(ls -l "$DIR_PROJECT/profiles" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_profiles" ]; then
+  for project_profile in $project_profiles
+  do
+    cp -R "$DIR_PROJECT/profiles/$project_profile" "$DIR_BUILD/web/profiles/$project_profile"
+    message_success "Copied profile $project_profile."
+  done
+else
+  message_warning "No project profiles available."
+fi
 
 # Copy modules.
-cp -R "$DIR_PROJECT/modules/custom" "$DIR_BUILD/web/sites/all/modules/custom"
-if [ $? -ne 1 ]; then
+project_modules=$(ls -l "$DIR_PROJECT/modules/custom" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_modules" ]; then
+  mkdir -p "$DIR_BUILD/web/sites/all/modules"
+  cp -R "$DIR_PROJECT/modules/custom" "$DIR_BUILD/web/sites/all/modules/custom"
   message_success "Copied custom modules."
 else
-  message_error "Could not copy custom modules."
+  message_warning "No project modules available."
 fi
 
 # Copy themes.
-cp -R "$DIR_PROJECT/themes/custom" "$DIR_BUILD/web/sites/all/themes/custom"
-if [ $? -ne 1 ]; then
+project_themes=$(ls -l "$DIR_PROJECT/themes/custom" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_themes" ]; then
+  mkdir -p "$DIR_BUILD/web/sites/all/themes"
+  cp -R "$DIR_PROJECT/themes/custom" "$DIR_BUILD/web/sites/all/themes/custom"
   message_success "Copied custom themes."
 else
-  message_error "Could not copy custom themes."
+  message_warning "No project themes available."
 fi
 
+# Copy libraries.
+project_libraries=$(ls -l "$DIR_PROJECT/libraries" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_libraries" ]; then
+  mkdir -p "$DIR_BUILD/web/sites/all/libraries"
+
+  for project_library in $project_libraries
+  do
+    cp -R "$DIR_PROJECT/libraries/$project_library" "$DIR_WEB/sites/all/libraries/$project_library"
+    message_success "Copied library $project_library."
+  done
+else
+  message_warning "No project libraries available."
+fi
 
 echo
--- config/install/drupal_make_after.sh
+++ config/install/drupal_make_after.sh
@@ -5,23 +5,52 @@
 ################################################################################
 
 
-# Add symlinks from the project folder.
-markup_h1 "Symlink custom modules and themes."
+# Add symlinks from the project directory.
+markup_h1 "Symlink project profiles, modules, themes and libraries."
+
+# Symlink profiles.
+project_profiles=$(ls -l "$DIR_PROJECT/profiles" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_profiles" ]; then
+  for project_profile in $project_profiles
+  do
+    ln -s "$DIR_PROJECT/profiles/$project_profile" "$DIR_WEB/profiles/$project_profile"
+    message_success "Symlinked profile $project_profile."
+  done
+else
+  message_warning "No project profiles available."
+fi
 
 # Symlink modules.
-if [ ! -L "$DIR_WEB/sites/all/modules/custom" ]; then
+project_modules=$(ls -l "$DIR_PROJECT/modules/custom" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_modules" ]; then
+  mkdir -p "$DIR_WEB/sites/all/modules"
   ln -s "$DIR_PROJECT/modules/custom" "$DIR_WEB/sites/all/modules/custom"
   message_success "Symlinked custom modules."
 else
-  message_warning "Symlink already exists for modules."
+  message_warning "No project modules available."
 fi
 
 # Symlink themes.
-if [ ! -L "$DIR_WEB/sites/all/themes/custom" ]; then
+project_themes=$(ls -l "$DIR_PROJECT/themes/custom" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_themes" ]; then
+  mkdir -p "$DIR_WEB/sites/all/themes"
   ln -s "$DIR_PROJECT/themes/custom" "$DIR_WEB/sites/all/themes/custom"
   message_success "Symlinked custom themes."
 else
-  message_warning "Symlink already exists for themes."
+  message_warning "No project themes available."
+fi
+
+# Symlink libraries.
+project_libraries=$(ls -l "$DIR_PROJECT/libraries" | grep "^d" | awk -F" " '{print $9}')
+if [ "$project_libraries" ]; then
+  mkdir -p "$DIR_WEB/sites/all/libraries"
+  for project_library in $project_libraries
+  do
+    ln -s "$DIR_PROJECT/libraries/$project_library" "$DIR_WEB/sites/all/libraries/$project_library"
+    message_success "Symlinked library $project_library."
+  done
+else
+  message_warning "No project libraries available."
 fi
 
 echo
